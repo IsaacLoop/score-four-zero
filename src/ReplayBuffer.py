21f4120f7dee152f446e89389aae2076aa667da5
Isaac <isaac.nivet@gmail.com>
2026-04-09 16:19:32 +0200

    oops! code mistake

--- src/ReplayBuffer.py
+++ src/ReplayBuffer.py
@@ -71,10 +71,10 @@ class ReplayBuffer:
             batch_size <= len(self.data)
         ), "Not enough examples in the buffer to sample a batch."
         batch = random.sample(self.data, batch_size)
-        batch = [apply_symmetry(b[0], b[1]) for b in batch]
+        symmetry_batch = [apply_symmetry(b[0], b[1]) for b in batch]
 
-        x_batch = np.stack([b[0] for b in batch])
-        pi_batch = np.stack([b[1] for b in batch])
+        x_batch = np.stack([b[0] for b in symmetry_batch])
+        pi_batch = np.stack([b[1] for b in symmetry_batch])
         z_batch = np.stack([b[2] for b in batch])
 
         return x_batch, pi_batch, z_batch
